792e9e6ea142956e8dc7a7cc450166dd3212e498
Thomas van Dam <thomas@seda.xyz>
2026-02-01 15:41:12 +0100

    fix: only consider the pathname for permalinks

--- src/lib/permalink.ts
+++ src/lib/permalink.ts
@@ -4,4 +4,7 @@ const PermalinkSchema = Schema.String.pipe(Schema.brand("Permalink"));
 const decodePermalink = Schema.decode(PermalinkSchema);
 
 export type Permalink = typeof PermalinkSchema.Type;
-export const getPermalink = (link: string) => decodePermalink(link.replace("https://soundcloud.com/", ""));
+export const getPermalink = (link: string) => {
+	const url = new URL(link);
+	return decodePermalink(url.pathname);
+};
